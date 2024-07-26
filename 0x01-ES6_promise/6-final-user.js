7d841340e252ce65ce4cc0d109f5f9a72a322d11
otega07 <okiriotega@gmail.com>
2024-07-26 08:45:11 +0100

    task updates

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  filename
+) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]).then((res) =>
+    res.map((o) => ({
+      status: o.status,
+      value: o.status === "fulfilled" ? o.value : String(o.reason),
+    }))
+  );
+}
