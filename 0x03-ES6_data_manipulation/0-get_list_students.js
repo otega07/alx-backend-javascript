af326f47d1fa09d9ea09190a664015e06f1ed326
otega07 <okiriotega@gmail.com>
2024-07-31 21:34:21 +0100

    adv

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,12 @@
-function getListStudents() {  
-    return [  
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },  
-        { id: 2, firstName: 'James', location: 'Columbia' },  
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }  
-    ];  
-}  
-
-export default getListStudents;
+/**
+ * Returns an array of student objects with their respective IDs, first names, and locations.
+ *
+ * @return {Array<Object>} An array of student objects.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
